fb2b9ced08b82d522624d892100d4a689db0a2df
Vera Kowalczuk <vera.kowalczuk@tum.de>
2023-11-28 23:25:10 +0100

    Test if removing all.min.css works to fix the font on RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -90,7 +90,7 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_css_files = [
     'custom.css',
-    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+    # "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
 ]
 
 
